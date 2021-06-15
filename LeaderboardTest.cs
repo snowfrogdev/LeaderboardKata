e8ca6636be9b070049046631256f2741bf86e94d
Philippe Vaillancourt <philippe.m.vaillancourt@gmail.com>
2021-06-15 16:05:37 -0400

    Add flexibility by making the score type generic

--- LeaderboardTest.cs
+++ LeaderboardTest.cs
@@ -11,23 +11,23 @@ namespace LeaderboardKata
         [Fact]
         public void Highscore_no_duplicates()
         {
-            var input = new List<Result>
+            var input = new List<Result<int>>
             {
-                new Result { Name = "Vader", Score = 12 },
-                new Result { Name = "Luke", Score = 15 },
-                new Result { Name = "Palpatine", Score = 5 },
-                new Result { Name = "Chewie", Score = 9 },
-                new Result { Name = "C3PO", Score = 2 },
+                new Result<int> { Name = "Vader", Score = 12 },
+                new Result<int> { Name = "Luke", Score = 15 },
+                new Result<int> { Name = "Palpatine", Score = 5 },
+                new Result<int> { Name = "Chewie", Score = 9 },
+                new Result<int> { Name = "C3PO", Score = 2 },
             };
 
 
-            var expected = new List<LeaderboardEntry>
+            var expected = new List<LeaderboardEntry<int>>
             {
-                new LeaderboardEntry { Result = new Result { Name = "Luke", Score = 15 }, Rank = 1 },
-                new LeaderboardEntry { Result = new Result { Name = "Vader", Score = 12 }, Rank = 2 },
-                new LeaderboardEntry { Result = new Result { Name = "Chewie", Score = 9 }, Rank = 3 },
-                new LeaderboardEntry { Result = new Result { Name = "Palpatine", Score = 5 }, Rank = 4 },
-                new LeaderboardEntry { Result = new Result { Name = "C3PO", Score = 2 }, Rank = 5 },
+                new LeaderboardEntry<int> { Result = new Result<int> { Name = "Luke", Score = 15 }, Rank = 1 },
+                new LeaderboardEntry<int> { Result = new Result<int> { Name = "Vader", Score = 12 }, Rank = 2 },
+                new LeaderboardEntry<int> { Result = new Result<int> { Name = "Chewie", Score = 9 }, Rank = 3 },
+                new LeaderboardEntry<int> { Result = new Result<int> { Name = "Palpatine", Score = 5 }, Rank = 4 },
+                new LeaderboardEntry<int> { Result = new Result<int> { Name = "C3PO", Score = 2 }, Rank = 5 },
             };
 
             var service = new LeaderboardService();
@@ -40,23 +40,23 @@ namespace LeaderboardKata
         [Fact]
         public void Highscore_with_duplicates()
         {
-            var input = new List<Result>
+            var input = new List<Result<int>>
             {
-                new Result { Name = "Vader", Score = 9 },
-                new Result { Name = "Luke", Score = 15 },
-                new Result { Name = "Palpatine", Score = 5 },
-                new Result { Name = "Chewie", Score = 9 },
-                new Result { Name = "C3PO", Score = 2 },
+                new Result<int> { Name = "Vader", Score = 9 },
+                new Result<int> { Name = "Luke", Score = 15 },
+                new Result<int> { Name = "Palpatine", Score = 5 },
+                new Result<int> { Name = "Chewie", Score = 9 },
+                new Result<int> { Name = "C3PO", Score = 2 },
             };
 
 
-            var expected = new List<LeaderboardEntry>
+            var expected = new List<LeaderboardEntry<int>>
             {
-                new LeaderboardEntry { Result = new Result { Name = "Luke", Score = 15 }, Rank = 1 },
-                new LeaderboardEntry { Result = new Result { Name = "Vader", Score = 9 }, Rank = 2 },
-                new LeaderboardEntry { Result = new Result { Name = "Chewie", Score = 9 }, Rank = 2 },
-                new LeaderboardEntry { Result = new Result { Name = "Palpatine", Score = 5 }, Rank = 4 },
-                new LeaderboardEntry { Result = new Result { Name = "C3PO", Score = 2 }, Rank = 5 },
+                new LeaderboardEntry<int> { Result = new Result<int> { Name = "Luke", Score = 15 }, Rank = 1 },
+                new LeaderboardEntry<int> { Result = new Result<int> { Name = "Vader", Score = 9 }, Rank = 2 },
+                new LeaderboardEntry<int> { Result = new Result<int> { Name = "Chewie", Score = 9 }, Rank = 2 },
+                new LeaderboardEntry<int> { Result = new Result<int> { Name = "Palpatine", Score = 5 }, Rank = 4 },
+                new LeaderboardEntry<int> { Result = new Result<int> { Name = "C3PO", Score = 2 }, Rank = 5 },
             };
 
             var service = new LeaderboardService();
@@ -69,23 +69,52 @@ namespace LeaderboardKata
         [Fact]
         public void Lowscore_with_duplicates()
         {
-            var input = new List<Result>
+            var input = new List<Result<int>>
             {
-                new Result { Name = "Vader", Score = 84 },
-                new Result { Name = "Luke", Score = 72 },
-                new Result { Name = "Palpatine", Score = 67 },
-                new Result { Name = "Chewie", Score = 84 },
-                new Result { Name = "C3PO", Score = 120 },
+                new Result<int> { Name = "Vader", Score = 84 },
+                new Result<int> { Name = "Luke", Score = 72 },
+                new Result<int> { Name = "Palpatine", Score = 67 },
+                new Result<int> { Name = "Chewie", Score = 84 },
+                new Result<int> { Name = "C3PO", Score = 120 },
             };
 
 
-            var expected = new List<LeaderboardEntry>
+            var expected = new List<LeaderboardEntry<int>>
             {
-                new LeaderboardEntry { Result = new Result { Name = "Palpatine", Score = 67 }, Rank = 1 },
-                new LeaderboardEntry { Result = new Result { Name = "Luke", Score = 72 }, Rank = 2 },
-                new LeaderboardEntry { Result = new Result { Name = "Vader", Score = 84 }, Rank = 3 },
-                new LeaderboardEntry { Result = new Result { Name = "Chewie", Score = 84 }, Rank = 3 },
-                new LeaderboardEntry { Result = new Result { Name = "C3PO", Score = 120 }, Rank = 5 },
+                new LeaderboardEntry<int> { Result = new Result<int> { Name = "Palpatine", Score = 67 }, Rank = 1 },
+                new LeaderboardEntry<int> { Result = new Result<int> { Name = "Luke", Score = 72 }, Rank = 2 },
+                new LeaderboardEntry<int> { Result = new Result<int> { Name = "Vader", Score = 84 }, Rank = 3 },
+                new LeaderboardEntry<int> { Result = new Result<int> { Name = "Chewie", Score = 84 }, Rank = 3 },
+                new LeaderboardEntry<int> { Result = new Result<int> { Name = "C3PO", Score = 120 }, Rank = 5 },
+            };
+
+            var service = LeaderboardService.ForLowScore();
+
+            var actual = service.GenerateLeaderboarEntries(input);
+
+            actual.Should().BeEquivalentTo(expected);
+        }
+
+        [Fact]
+        public void Lowscore_with_duplicates_and_IComparable_score_type()
+        {
+            var input = new List<Result<NonsensicalScore>>
+            {
+                new Result<NonsensicalScore> { Name = "Vader", Score = NonsensicalScore.Bad },
+                new Result<NonsensicalScore> { Name = "Luke", Score = NonsensicalScore.Awesome },
+                new Result<NonsensicalScore> { Name = "Palpatine", Score = NonsensicalScore.Bad },
+                new Result<NonsensicalScore> { Name = "Chewie", Score = NonsensicalScore.Meh },
+                new Result<NonsensicalScore> { Name = "C3PO", Score = NonsensicalScore.ICantEven },
+            };
+
+
+            var expected = new List<LeaderboardEntry<NonsensicalScore>>
+            {
+                new LeaderboardEntry<NonsensicalScore> { Result = new Result<NonsensicalScore> { Name = "C3PO", Score = NonsensicalScore.ICantEven }, Rank = 1 },
+                new LeaderboardEntry<NonsensicalScore> { Result = new Result<NonsensicalScore> { Name = "Vader", Score = NonsensicalScore.Bad }, Rank = 2 },
+                new LeaderboardEntry<NonsensicalScore> { Result = new Result<NonsensicalScore> { Name = "Palpatine", Score = NonsensicalScore.Bad }, Rank = 2 },
+                new LeaderboardEntry<NonsensicalScore> { Result = new Result<NonsensicalScore> { Name = "Chewie", Score = NonsensicalScore.Meh }, Rank = 4 },
+                new LeaderboardEntry<NonsensicalScore> { Result = new Result<NonsensicalScore> { Name = "Luke", Score = NonsensicalScore.Awesome }, Rank = 5 },
             };
 
             var service = LeaderboardService.ForLowScore();
@@ -96,15 +125,76 @@ namespace LeaderboardKata
         }
     }
 
-    public class Result
+    public class NonsensicalScore : IComparable<NonsensicalScore>, IEquatable<NonsensicalScore>
     {
-        public string Name { get; set; }
-        public int Score { get; set; }
+        private NonsensicalScore() { }
+
+        public string Value { get; private set; }
+
+        public static NonsensicalScore Awesome => new NonsensicalScore { Value = "Awesome" };
+        public static NonsensicalScore Great => new NonsensicalScore { Value = "Great" };
+        public static NonsensicalScore Good => new NonsensicalScore { Value = "Good" };
+        public static NonsensicalScore Meh => new NonsensicalScore { Value = "Meh" };
+        public static NonsensicalScore Bad => new NonsensicalScore { Value = "Bad" };
+        public static NonsensicalScore OMG => new NonsensicalScore { Value = "OMG" };
+        public static NonsensicalScore ICantEven => new NonsensicalScore { Value = "I Can't Even" };
+
+        private static readonly Dictionary<string, int> ScoreMap = new()
+        {
+            ["I Can't Even"] = 0,
+            ["OMG"] = 1,
+            ["Bad"] = 2,
+            ["Meh"] = 3,
+            ["Good"] = 4,
+            ["Great"] = 5,
+            ["Awesome"] = 6,
+        };
+
+        public int CompareTo(NonsensicalScore other)
+        {
+            return ScoreMap[Value] - ScoreMap[other.Value];
+        }
+
+        public bool Equals(NonsensicalScore other)
+        {
+            return CompareTo(other) == 0;
+        }
+
+        public static bool operator <(NonsensicalScore left, NonsensicalScore right)
+        {
+            return left.CompareTo(right) < 0;
+        }
+
+        public static bool operator <=(NonsensicalScore left, NonsensicalScore right)
+        {
+            return left.CompareTo(right) <= 0;
+        }
+
+        public static bool operator >(NonsensicalScore left, NonsensicalScore right)
+        {
+            return left.CompareTo(right) > 0;
+        }
+
+        public static bool operator >=(NonsensicalScore left, NonsensicalScore right)
+        {
+            return left.CompareTo(right) >= 0;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as NonsensicalScore);
+        }
     }
 
-    public class LeaderboardEntry
+    public class Result<T> where T : IComparable<T>
     {
-        public Result Result { get; set; }
+        public string Name { get; set; }
+        public T Score { get; set; }
+    }
+
+    public class LeaderboardEntry<T> where T : IComparable<T>
+    {
+        public Result<T> Result { get; set; }
         public int Rank { get; set; }
     }
 
@@ -117,22 +207,22 @@ namespace LeaderboardKata
             return new LeaderboardService { IsLowScore = true };
         }
 
-        public IEnumerable<LeaderboardEntry> GenerateLeaderboarEntries(List<Result> results)
+        public IEnumerable<LeaderboardEntry<T>> GenerateLeaderboarEntries<T> (List<Result<T>> results) where T : IComparable<T>
         {
             var sortedResults = SortResults(results);
 
-            var entries = new List<LeaderboardEntry>();
+            var entries = new List<LeaderboardEntry<T>>();
 
             int count = 0;
-            int? previousScore = null;
+            T previousScore = default;
             int previousRank = 0;
 
             foreach (var result in sortedResults)
             {
                 count++;
 
-                var rank = result.Score != previousScore ? count : previousRank;
-                var entry = new LeaderboardEntry { Result = result, Rank = rank };
+                var rank = previousScore is null || !result.Score.Equals(previousScore) ? count : previousRank;
+                var entry = new LeaderboardEntry<T> { Result = result, Rank = rank };
                 entries.Add(entry);
 
                 previousScore = result.Score;
@@ -142,7 +232,7 @@ namespace LeaderboardKata
             return entries;
         }
 
-        private IOrderedEnumerable<Result> SortResults(List<Result> results)
+        private IOrderedEnumerable<Result<T>> SortResults<T>(List<Result<T>> results) where T : IComparable<T>
         {
             return IsLowScore ? results.OrderBy(result => result.Score) : results.OrderByDescending(result => result.Score);
         }
